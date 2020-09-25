a17e71aae514641a54c08deb39eef476aa4d154f
Matewos Araya <arayamatewos@gmail.com>
2020-09-25 00:15:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args)} 
+   boolean integer = false && true;
+   System.out.println(false || true);
+   System.out.println(false || true);
+   System.out.println(false || true);
+   System.out.println(false || true);
+    
+  }
+}
